4f474c14bc1ecd1a4b51714853f1b06a29fb4236
batainem96 <batainem96@gmail.com>
2021-09-16 10:37:43 -0500

    Updated profile settings page to be more minimalist and sleek

--- src/components/UserProfileComponent.tsx
+++ src/components/UserProfileComponent.tsx
@@ -1,4 +1,4 @@
-import { Button, Container, responsiveFontSizes, TextField, Typography } from "@material-ui/core";
+import { Button, Container, InputAdornment, responsiveFontSizes, TextField, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
 import { useState, useEffect } from "react";
 import { Principal } from "../dtos/principal";
@@ -10,18 +10,23 @@ interface IUserProfile{
     setCurrentUser: (nextUser: Principal | undefined) => void
 }
 
+const FAINTGREY = '#9b9b9b';
+
 const useStyles = makeStyles({
     profileContainer: {
         textAlign: 'center',
         justifyContent: 'center', 
-        marginTop: '1rem',
-        marginBottom: '3rem',
-        marginLeft: '20rem',
-        marginRight: '20rem',
-        border: 'double', 
-        borderColor: '#4b6fe4',
+        // marginTop: '1rem',
+        // marginBottom: '3rem',
+        // marginLeft: '20rem',
+        // marginRight: '20rem',
+        border: `solid ${FAINTGREY}`,
         borderRadius: '12px',
-        borderWidth: '5px 20px',
+        borderWidth: '1px',
+    },
+
+    profileEntry: {
+        width: '50%'
     }
 });
 
@@ -70,44 +75,100 @@ function UserProfileComponent(props: IUserProfile){
     return (
 
         <>
-            <div id="register-component" className={classes.profileContainer}>
+            <Container fixed maxWidth='md' id="register-component" className={classes.profileContainer}>
                 <br/>
-                <Typography align="center" variant="h4">Your Profile!</Typography>
-                
-                <p>
-                    Name: {userData.firstName} {userData.lastName}
-                </p>
-                <Button 
-                    onClick={handleNameChange}
-                    variant="contained"
-                    color="primary"
-                    size="small"> Edit Name </Button> <br/><br/>
-
-
-                <p>Email: {userData.email}</p>
-                <Button 
-                    onClick={handleEmailChange}
-                    variant="contained"
-                    color="primary"
-                    size="small"> Change Email </Button> <br/><br/>
-
-                <p>Username: {userData.username}</p>
-                <Button 
-                    onClick={handleUsernameChange}
-                    variant="contained"
-                    color="primary"
-                    size="small"> Change Username </Button> <br/><br/>
-
-                <Button 
-                    onClick={handlePassChange}
-                    variant="contained"
-                    color="primary"
-                    size="small"> Change Password </Button>
+                <Typography align="center" variant="h4">Profile Settings</Typography>
 
                 <br/><br/>
 
+                <TextField
+                    id="standard-read-only-input"
+                    label="Name"
+                    className={classes.profileEntry}
+                    value={userData.firstName + ' ' + userData.lastName}
+                    InputProps={{
+                        readOnly: true,
+                        endAdornment: (
+                            <InputAdornment position="end">
+                                <Button 
+                                    onClick={handleNameChange}
+                                    color="primary"
+                                    size="small"> Edit
+                                </Button>
+                            </InputAdornment>
+                          )
+                    }}
+                />
 
-            </div>
+                <br/><br/>
+
+                <TextField
+                    id="standard-read-only-input"
+                    label="Email"
+                    className={classes.profileEntry}
+                    value={userData.email}
+                    InputProps={{
+                        readOnly: true,
+                        endAdornment: (
+                            <InputAdornment position="end">
+                                <Button 
+                                    onClick={handleEmailChange}
+                                    color="primary"
+                                    size="small"> Edit
+                                </Button>
+                            </InputAdornment>
+                          )
+                    }}
+                />
+
+                <br/><br/>
+
+                <TextField
+                    id="standard-read-only-input"
+                    label="Username"
+                    className={classes.profileEntry}
+                    value={userData.username}
+                    InputProps={{
+                        readOnly: true,
+                        endAdornment: (
+                            <InputAdornment position="end">
+                                <Button 
+                                    onClick={handleUsernameChange}
+                                    color="primary"
+                                    size="small"> Edit
+                                </Button>
+                            </InputAdornment>
+                          )
+                    }}
+                />
+
+                <br/><br/>
+
+                <TextField
+                    id="standard-read-only-input"
+                    label="Password"
+                    type="password"
+                    className={classes.profileEntry}
+                    value={userData.username}
+                    InputProps={{
+                        readOnly: true,
+                        endAdornment: (
+                            <InputAdornment position="end">
+                                <Button 
+                                    onClick={handlePassChange}
+                                    color="primary"
+                                    size="small"> Edit
+                                </Button>
+                            </InputAdornment>
+                          )
+                    }}
+                />
+
+                <br/><br/>
+                <br/><br/>
+                <br/><br/>
+
+            </Container>
         
         </>
     );
