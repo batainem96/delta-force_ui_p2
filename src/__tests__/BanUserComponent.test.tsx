6b4ef391ed45d174d6096f58a8d66a39bf7db13c
Robert Ni <robertni@outlook.fr>
2021-09-17 19:27:40 -0400

    added some tests for BanUserComponent

---
+++ src/__tests__/BanUserComponent.test.tsx
@@ -0,0 +1,97 @@
+import {mount, shallow} from "enzyme";
+import BanUserComponent from "../components/BanUserComponent";
+import {TextField, Typography} from "@material-ui/core";
+import {banUser} from "../remote/user-service";
+import SuccessMessageComponent from "../components/SuccessMessageComponent";
+import {Alert} from "@material-ui/lab";
+import ErrorMessageComponent from "../components/ErrorMessageComponent";
+import {act} from "@testing-library/react";
+
+const mockHistoryPush = jest.fn();
+
+jest.mock("react-router-dom", () => ({
+    ...jest.requireActual("react-router-dom"),
+    useHistory: () => ({
+        push: mockHistoryPush
+    })
+}));
+
+jest.mock("../remote/user-service");
+
+describe("BanUserComponent Test Suite", () => {
+
+    afterEach(() => {
+       jest.resetAllMocks();
+    });
+
+    it("renders successfully", () => {
+        let mockUser = {
+            id: "valid",
+            username: "valid",
+            token: "valid",
+            role: "valid",
+            favTopics: []
+        };
+        let setMockUserFn = jest.fn();
+
+        const wrapper = shallow(<BanUserComponent currentUser={mockUser} setCurrentUser={setMockUserFn} />);
+
+        expect(wrapper).toBeTruthy();
+    });
+
+    it("renders the correct header", () => {
+        let mockUser = {
+            id: "valid",
+            username: "valid",
+            token: "valid",
+            role: "valid",
+            favTopics: []
+        };
+        let setMockUserFn = jest.fn();
+
+        const wrapper = shallow(<BanUserComponent currentUser={mockUser} setCurrentUser={setMockUserFn} />);
+        const expectedHeader = <Typography align="center" variant="h4">Banning a user...</Typography>;
+
+        expect(wrapper.contains(expectedHeader)).toBe(true);
+    });
+
+    it("displays empty fields on first render", () => {
+        let mockUser = {
+            id: "valid",
+            username: "valid",
+            token: "valid",
+            role: "valid",
+            favTopics: []
+        };
+        let setMockUserFn = jest.fn();
+
+        const wrapper = shallow(<BanUserComponent currentUser={mockUser} setCurrentUser={setMockUserFn} />);
+
+        let usernameInput = wrapper.find("#username").at(0);
+
+        expect(usernameInput.text()).toBe("");
+    });
+
+    it("bans a user when provided valid username", () => {
+        let mockUser = {
+            id: "valid",
+            username: "valid",
+            token: "valid",
+            role: "valid",
+            favTopics: []
+        };
+        let setMockUserFn = jest.fn();
+
+        const wrapper = mount(<BanUserComponent currentUser={mockUser} setCurrentUser={setMockUserFn} />);
+
+        let username = wrapper.find("#username").at(0);
+        let banButton = wrapper.find("#edit-button").at(0);
+
+        act(() => {
+            username.simulate("change", {target: {name: "username", value: "test-username"}});
+            banButton.simulate("click");
+        });
+
+        expect(banUser).toBeCalledTimes(1);
+    });
+});
