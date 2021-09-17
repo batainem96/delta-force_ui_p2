01b1416c33fa5bc43637c0c01daea702b01f6731
hpuf <csmcdn@gmail.com>
2021-09-17 13:30:25 -0500

    added useEffect in appState to check localstorage for jwt and attempting to persist user on refresh

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import {BrowserRouter, Redirect, Route, Switch} from "react-router-dom";
+import jwt from 'jwt-decode';
 import DashboardComponent from './components/DashboardComponent';
 import LoginComponent from './components/LoginComponent';
 import NavbarComponent from "./components/NavbarComponent";
 import RegisterComponent from './components/RegisterComponent';
 import { Principal } from './dtos/principal';
 import UserProfileComponent from './components/UserProfileComponent';
-
 import { ArticleQuery } from './models/acticle-query';
-
 import EditProfileComponent from './components/EditProfileComponent';
 import EditEmailComponent from './components/EditEmailComponent';
 import EditUsernameComponent from './components/EditUsernameComponent';
 import EditPassComponent from './components/EditPassComponent';
 import AdminControlPanelComponent from './components/AdminControlPanelComponent';
 import BanUserComponent from './components/BanUserComponent';
+import { PrincipalJWT } from './dtos/jwt-dto';
 
 function App() {
 
   let [currentUser, setCurrentUser] = useState(undefined as Principal | undefined);
   let [searchQuery, setSearchQuery] = useState(undefined as ArticleQuery | undefined);
+
+  useEffect(() => {
+      const loggedInUser = localStorage.getItem('api-token');
+      if (loggedInUser){
+        let user : PrincipalJWT = jwt(loggedInUser);
+        let authUser : Principal = new Principal(user.jti, user.sub, user.role, loggedInUser, []);
+        console.log(authUser);
+        setCurrentUser(authUser);
+      }
+  }, []);
 
   return (
       <BrowserRouter>
--- src/remote/auth-service.ts
+++ src/remote/auth-service.ts
@@ -1,6 +1,7 @@
 import {Credentials} from "../dtos/credentials";
 import {deltaforceClient} from "./deltaforce-client";
 import {Principal} from "../dtos/principal";
+import jwt from 'jwt-decode';
 
 
 export const authenticate = async (credentials: Credentials) => {
@@ -24,4 +25,13 @@ export const logout = (setCurrentUser: (nextUser: Principal | undefined) => void
     deltaforceClient.defaults.headers["Authorization"] = undefined;
     localStorage.removeItem('api-token')
     setCurrentUser(undefined);
+}
+
+export const maintainUser = (setCurrentUser: (authUser: Principal | undefined) => Principal) => {
+    const loggedInUser = localStorage.getItem('api-token');
+      if (loggedInUser){
+        let user : Principal = jwt(loggedInUser);
+        console.log(user);
+        return user;
+      }
 }
